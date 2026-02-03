140e0c7a1243fc726777704db9a7dbe09112ed7d
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2026-02-03 04:40:29 +0100

    Fix component references

--- SyncAll.cmd
+++ SyncAll.cmd
@@ -1,5 +1,5 @@
 @echo off
-xcopy /y /d H:\blueprints\automation\edwardtfn\nspanel_blueprint_dev.yaml .\nspanel_blueprint.yaml
+xcopy /y /d H:\blueprints\automation\edwardtfn\nspanel_easy_blueprint_dev.yaml .\nspanel_easy_blueprint.yaml
 if errorlevel 1 timeout /t 10
 xcopy /y /d H:\esphome\packages\edwardtfn\NSPanel-Easy\nspanel_esphome*.yaml .
 if errorlevel 1 timeout /t 10
@@ -22,8 +22,6 @@ if errorlevel 1 timeout /t 10
 python dev\Nextion2Text.py -i hmi\nspanel_CJK_us.HMI -o hmi\dev\nspanel_CJK_us_code -p visual unknown
 if errorlevel 1 timeout /t 10
 python dev\Nextion2Text.py -i hmi\nspanel_CJK_us_land.HMI -o hmi\dev\nspanel_CJK_us_land_code -p visual unknown
-if errorlevel 1 timeout /t 10
-python dev\Nextion2Text.py -i hmi\nspanel_blank.HMI -o hmi\dev\nspanel_blank_code -p visual unknown
 if errorlevel 1 timeout /t 10
 xcopy /y /d hmi\*.tft H:\www\nspanel\dev
 if errorlevel 1 timeout /t 10
--- SyncNextion2TXT.cmd
+++ SyncNextion2TXT.cmd
@@ -11,8 +11,6 @@ python dev\Nextion2Text.py -i hmi\nspanel_CJK_us.HMI -o hmi\dev\nspanel_CJK_us_c
 if errorlevel 1 timeout /t 10
 python dev\Nextion2Text.py -i hmi\nspanel_CJK_us_land.HMI -o hmi\dev\nspanel_CJK_us_land_code -p visual unknown
 if errorlevel 1 timeout /t 10
-python dev\Nextion2Text.py -i hmi\nspanel_blank.HMI -o hmi\dev\nspanel_blank_code -p visual unknown
-if errorlevel 1 timeout /t 10
 xcopy /y /d hmi\*.tft H:\www\nspanel\dev
 if errorlevel 1 timeout /t 10
  
